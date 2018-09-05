2c79204da1acd5c7e9ac4f19b65210555fea8568
Yan Gorobtsov <YachaGorobtsov@gmail.com>
2018-09-05 21:08:30 +0300

    Orangify tree
    * Stage all configs in BoardConfig
    * Add support extra-languages
    * Add OrangeFox kernel script

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,6 +16,92 @@
 
 LOCAL_PATH := device/xiaomi/oxygen
 
-# Device board elements
-include $(LOCAL_PATH)/PlatformConfig.mk
-include $(LOCAL_PATH)/board/*.mk
+# Platform
+TARGET_BOOTLOADER_BOARD_NAME := MSM8953
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+TARGET_BOARD_PLATFORM := msm8953
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
+
+TARGET_BOARD_SUFFIX := _64
+TARGET_USES_64_BIT_BINDER := true
+
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+
+# Kernel informations
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 androidboot.selinux=permissive
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET := 0x01000000
+
+# OrangeFox Kernel
+ifeq ($(FOX_BUILD_FULL_KERNEL_SOURCES),)
+FOX_BUILD_FULL_KERNEL_SOURCES := 1
+endif
+
+ifeq ($(FOX_BUILD_FULL_KERNEL_SOURCES),1)
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_CONFIG := oxygen_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/oxygen
+else
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilds/kernel
+PRODUCT_COPY_FILES += \
+    $(TARGET_PREBUILT_KERNEL):kernel
+endif
+# OrangeFox Kernel end
+
+# Kernel informations
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 androidboot.selinux=permissive
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET := 0x01000000
+
+# Prebuilt kernel
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilts/kernel
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 65536000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 65536000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4194304000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 8388591616 # (58846081024-16384)
+BOARD_CACHEIMAGE_PARTITION_SIZE := 262144000
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 65536000
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# TWRP Configuration
+TW_THEME := portrait_hdpi
+TW_INCLUDE_CRYPTO := true
+TW_MAX_BRIGHTNESS := 4095
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_SCREEN_BLANK_ON_BOOT := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/twrp.fstab
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TWRP_INCLUDE_LOGCAT := true
+TW_EXTRA_LANGUAGES := true
--- board/kernel.mk
+++
@@ -1,9 +0,0 @@
-# Kernel informations
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 androidboot.selinux=permissive
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET := 0x01000000
-
-# Prebuilt kernel
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilts/kernel
